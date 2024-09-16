1748aab7814b287a4ca2fddffb4653c2b7a2e3a1
sandykhot10 <sandykhot10@gmail.com>
2024-09-16 14:18:24 +0530

    changing the logo

--- src/components/ Navbar.js
+++ src/components/ Navbar.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css'; // Create this CSS file for custom styles
+import logo from '/Users/sandykhot/Desktop/gym/gym-website/src/assets/fitness.png'; // Import the new logo
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/">
-          <img src="path/to/logo.png" alt="Gym Logo" className="logo" /> {/* Replace with your logo */}
-          GymZone
-        </Link>
+      <Link className="navbar-brand" to="/">
+        <img src={logo} alt="Vighnesh Fitness Logo" className="logo" /> {/* Updated logo */}
+        Vighnesh Fitness
+      </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
